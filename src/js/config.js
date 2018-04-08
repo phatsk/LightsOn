8e82d8a76c95f9d001f24e2e54091cfaa4b9a07c
Zach Owen <phatsk@gmail.com>
2018-04-08 01:31:29 -0400

    Update configuration

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,39 @@
 module.exports = [{
 	"type": "heading",
-	"defaultValue": "Example Header Item"
+	"defaultValue": "LightsOn Configuration"
 },{
-	"type": "text",
-	"defaultValue": "Example text item."
+	"type": "section",
+	"items": [{
+		"type": "input",
+		"messageKey": "email",
+		"defaultValue": "",
+		"label": "Email Address",
+		"attributes": {
+			"placeholder": "eg: name@domain.com",
+			"type": "email"
+		}
+	},{
+		"type": "text",
+		"defaultValue": "Your VeSync Account E-mail"
+	},{
+		"type": "input",
+		"messageKey": "password",
+		"defaultValue": "",
+		"label": "Password",
+		"attributes": {
+			"type": "password"
+		}
+	},{
+		"type": "text",
+		"defaultValue": "Your VeSync Account Password"
+	}]
+},{
+	"type": "section",
+	"items": [{
+		"type": "header",
+		"defaultValue": "Disclaimer"
+	},{
+		"type": "text",
+		"defaultValue": "Your account email and password are only used to authorize you with VeSync's servers. Your infromation is not stored."
+	}]
 }];
