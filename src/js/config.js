ce4376e73e60f9288553ac6b7f602cdf174915ac
Zach Owen <phatsk@gmail.com>
2018-04-08 01:46:15 -0400

    Fix configuration

--- src/js/config.js
+++ src/js/config.js
@@ -1,35 +1,32 @@
 module.exports = [{
-	"type": "heading",
-	"defaultValue": "LightsOn Configuration"
+  "type": "heading",
+  "defaultValue": "LightsOn Configuration"
 },{
-	"type": "section",
-	"items": [{
-		"type": "input",
-		"messageKey": "email",
-		"defaultValue": "",
-		"label": "Email Address",
-		"attributes": {
-			"placeholder": "eg: name@domain.com",
-			"type": "email"
-		}
-	},{
-		"type": "input",
-		"messageKey": "password",
-		"defaultValue": "",
-		"label": "Password",
-		"attributes": {
-			"type": "password"
-		}
-	},{
-		"type": "text",
-		"defaultValue": "Your VeSync Account Password"
-	}]
+  "type": "section",
+  "items": [{
+    "type": "input",
+    "messageKey": "email",
+    "defaultValue": "",
+    "label": "Email Address",
+    "attributes": {
+      "placeholder": "eg: name@domain.com",
+      "type": "email"
+    }
+  },{
+    "type": "input",
+    "messageKey": "password",
+    "defaultValue": "",
+    "label": "Password",
+    "attributes": {
+      "type": "password"
+    }
+  }]
 },{
-	"type": "header",
-	"defaultValue": "Disclaimer"
+  "type": "heading",
+  "defaultValue": "Disclaimer"
 },{
-	"type": "text",
-	"defaultValue": "Your account email and password are only used to authorize you with VeSync's servers. Your infromation is not stored."
+  "type": "text",
+  "defaultValue": "Your account email and password are only used to authorize you with VeSync's servers. Your infromation is not stored."
 },{
   "type": "submit",
   "defaultValue": "Authorize"
