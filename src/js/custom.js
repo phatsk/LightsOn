51117554eb8307a09792e4d243b37d0dd4b2d335
Zach Owen <phatsk@gmail.com>
2018-04-10 00:04:28 -0400

    Super confused

--- src/js/custom.js
+++ src/js/custom.js
@@ -5,21 +5,4 @@ module.exports = function(minified) {
   var HTML = minified.HTML;
 
   console.log(JSON.stringify(clayConfig));
-  clayConfig.on(clayConfig.EVENTS.AFTER_BUILD, function() {
-    var vsEmail = clayConfig.getItemByAppKey('vsEmail');
-    var vsPass  = clayConfig.getItemByAppKey('vsEmail');
-
-    console.log([vsEmail,vsPass])
-
-    // Set the value of an item based on the userData
-    /*
-    $.request('get', 'https://some.cool/api', {token: clayConfig.meta.userData.token})
-      .then(function(result) {
-	// Do something interesting with the data from the server
-      })
-      .error(function(status, statusText, responseText) {
-	// Handle the error
-      });
-  */
-  });
 };
